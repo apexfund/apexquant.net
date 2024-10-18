12956603111605c90c3f5230c88e0f824d7dafc8
Saipraneeth Mukku <saipmukku@gmail.com>
2024-10-17 22:24:19 -0400

    Made the basic layout for the landing page and added Framer Motion

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,70 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Text, ScaleFade, Image, Card, CardBody } from '@chakra-ui/react';
+import { motion } from "framer-motion";
 import '../App.jsx';
 
 function HomePage() {
 
     return (
-        <Flex direction="row" padding={6}>
-            <Flex direction="column" justify="flex-start">
-                <Heading size="3xl">Apex Quant</Heading>
-                <Heading size="xl">A Quantitative Finance Club at UMD</Heading>
+        <ScaleFade in={true} initalScale={0} unmountOnExit={true}>
+            <Flex direction="column">
+                <Flex direction="row" padding={6}>
+                    <Flex direction="column" justify="center" align="center" flex={1} padding={5}>
+                        <motion.Box whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.03 }}>
+                            <Heading size="4xl" pb={5}>Apex Quant</Heading>
+                        </motion.Box>
+                        <motion.Box whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.03 }}>
+                            <Heading size="xl" pb={12}>A Quantitative Finance Club at UMD</Heading>
+                        </motion.Box>
+                        <motion.Box whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.03 }}>
+                            <Text fontSize="xl">
+                                We are a quantitative finance group at UMD consisting 
+                                of a diverse group of students. Our purpose is to research 
+                                and refine trading strategies we believe are effective so 
+                                as to grow and maintain a fruitful and market-exploiting 
+                                stock portfolio.
+                            </Text>
+                        </motion.Box>
+                    </Flex>
+                    <Flex flex={1}>
+                        <motion.Box whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.03 }}>
+                            <Image src="stockstock.png" borderRadius="full" alt="Stock photo of stocks"/>
+                        </motion.Box>
+                    </Flex>
+                </Flex>
+                <Flex direction="row" justify="space-between">
+                    <motion.Box whileHover={{ scale: 1.01 }} whileTap={{ scale: 1.02 }}>
+                        <Card bg="#778ca3" variant="elevated" size="lg">
+                            <CardBody>
+                                <Flex direction="column" align="center">
+                                    <Text fontSize="3xl">22</Text>
+                                    <Text fontSize="lg">Members</Text>
+                                </Flex>
+                            </CardBody>
+                        </Card>
+                    </motion.Box>
+                    <motion.Box whileHover={{ scale: 1.01 }} whileTap={{ scale: 1.02 }}>
+                        <Card bg="#778ca3" variant="elevated" size="lg">
+                            <CardBody>
+                                <Flex direction="column" align="center">
+                                    <Text fontSize="3xl">3</Text>
+                                    <Text fontSize="lg">Teams</Text>
+                                </Flex>
+                            </CardBody>
+                        </Card>
+                    </motion.Box>
+                    <motion.Box whileHover={{ scale: 1.01 }} whileTap={{ scale: 1.02 }}>
+                        <Card bg="#778ca3" variant="elevated" size="lg">
+                            <CardBody>
+                                <Flex direction="column" align="center">
+                                    <Text fontSize="3xl">8</Text>
+                                    <Text fontSize="lg">Years since founding</Text>
+                                </Flex>
+                            </CardBody>
+                        </Card>
+                    </motion.Box>
+                </Flex>
             </Flex>
-        </Flex>
+        </ScaleFade>
     )
 }
 
